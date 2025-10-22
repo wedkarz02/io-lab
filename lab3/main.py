f8a80d670c2254690b245b16232723684187c1a9
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-10-22 18:56:39 +0200

    lab3: iris classification

--- lab3/main.py
+++ lab3/main.py
@@ -1,9 +1,37 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def split_dataset():
+    df = pd.read_csv("data/iris_big.csv")
+    return train_test_split(df.values, train_size=0.7, random_state=288568)
+
+
+def classify_iris(sl, sw, pl, pw):
+    if sl > 4:
+        return "setosa"
+    elif pl <= 5:
+        return "virginica"
+    else:
+        return "versicolor"
 
 
 def main():
-    df = pd.read_csv("data/iris_big.csv")
-    print(df)
+    (train_set, test_set) = split_dataset()
+    good_pred = 0
+    lenght = test_set.shape[0]
+
+    for i in range(lenght):
+        if (
+            classify_iris(
+                test_set[i][0], test_set[i][1], test_set[i][2], test_set[i][3]
+            )
+            == test_set[i][4]
+        ):
+            good_pred += 1
+
+    print(good_pred)
+    print(good_pred / test_set.shape[0] * 100, "%")
 
 
 if __name__ == "__main__":
