ee440fd6c40e8baf86ee31371dd9de27b4c5942c
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-10-28 18:27:18 +0100

    zad1: klasyfikacja przez czlowieka

--- lab3/main.py
+++ lab3/main.py
@@ -2,37 +2,42 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def split_dataset():
-    df = pd.read_csv("data/iris_big.csv")
+def in_range(num, low, high):
+    return low <= num <= high
+
+
+def split_dataset(file_name):
+    df = pd.read_csv(file_name)
     return train_test_split(df.values, train_size=0.7, random_state=288568)
 
 
-def classify_iris(sl, sw, pl, pw):
-    if sl > 4:
+def classify_iris(row):
+    # sl, sw, pl, pw = row
+    _, _, pl, _ = row
+
+    if in_range(pl, 1.2, 1.8):
         return "setosa"
-    elif pl <= 5:
+    elif in_range(pl, 4.1, 4.9):
         return "virginica"
     else:
         return "versicolor"
 
 
 def main():
-    (train_set, test_set) = split_dataset()
-    good_pred = 0
-    lenght = test_set.shape[0]
+    _, test_set = split_dataset("data/iris_big.csv")
 
-    for i in range(lenght):
-        if (
-            classify_iris(
-                test_set[i][0], test_set[i][1], test_set[i][2], test_set[i][3]
-            )
-            == test_set[i][4]
-        ):
+    length = len(test_set)
+    good_pred = 0
+
+    for i in range(length):
+        if classify_iris(test_set[i][:4]) == test_set[i][4]:
             good_pred += 1
 
     print(good_pred)
-    print(good_pred / test_set.shape[0] * 100, "%")
+    print(good_pred / length * 100, "%")
 
 
 if __name__ == "__main__":
+    # df = pd.read_csv("data/iris_big.csv")
+    # print(df.groupby("target_name").sample(3))
     main()
