365f73aa27ef744b76722fdf8101cde8dd720dd7
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-10-28 18:52:36 +0100

    zad1: namespaces tasks

--- lab3/main.py
+++ lab3/main.py
@@ -2,42 +2,40 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def in_range(num, low, high):
-    return low <= num <= high
-
-
 def split_dataset(file_name):
     df = pd.read_csv(file_name)
     return train_test_split(df.values, train_size=0.7, random_state=288568)
 
 
-def classify_iris(row):
-    # sl, sw, pl, pw = row
-    _, _, pl, pw = row
+class Zad1:
+    @staticmethod
+    def classify_iris(row):
+        # sl, sw, pl, pw = row
+        _, _, pl, pw = row
 
-    if pl < 4:
-        return "setosa"
-    elif pw < 1:
-        return "virginica"
-    else:
-        return "versicolor"
+        if pw < 0.5:
+            return "setosa"
+        elif pl > 4:
+            return "virginica"
+        else:
+            return "versicolor"
 
+    @staticmethod
+    def main():
+        _, test_set = split_dataset("data/iris_big.csv")
 
-def main():
-    _, test_set = split_dataset("data/iris_big.csv")
+        length = len(test_set)
+        good_pred = 0
 
-    length = len(test_set)
-    good_pred = 0
+        for i in range(length):
+            if Zad1.classify_iris(test_set[i][:4]) == test_set[i][4]:
+                good_pred += 1
 
-    for i in range(length):
-        if classify_iris(test_set[i][:4]) == test_set[i][4]:
-            good_pred += 1
-
-    print(good_pred)
-    print(good_pred / length * 100, "%")
+        print(good_pred)
+        print(good_pred / length * 100, "%")
 
 
 if __name__ == "__main__":
     # df = pd.read_csv("data/iris_big.csv")
-    # print(df.groupby("target_name").sample(3))
-    main()
+    # print(df.groupby("target_name").sample(10))
+    Zad1().main()
