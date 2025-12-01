1c7a05a6e51c206c4de530e31eebcb8cbe6ae4bb
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-12-01 17:51:52 +0100

    lab8/zad2

--- lab8/aco-tsp.py
+++ lab8/aco-tsp.py
@@ -1,28 +1,57 @@
 import matplotlib.pyplot as plt
 import random
+import sys
 
 from aco import AntColony
 
-
 plt.style.use("dark_background")
 
-
-COORDS = (
-    (20, 52),
-    (43, 50),
-    (20, 84),
-    (70, 65),
-    (29, 90),
-    (87, 83),
-    (73, 23)
-)
+# COORDS = ((20, 52), (43, 50), (20, 84), (70, 65), (29, 90), (87, 83), (73, 23))
 
 
+def random_coords(count, min, max):
+    return tuple(
+        [(random.randrange(min, max), random.randrange(min, max)) for _ in range(count)]
+    )
 
 
-def random_coord():
-    r = random.randint(0, len(COORDS))
-    return r
+coords_len = 15
+COORDS = random_coords(coords_len, 0, 100)
+
+ant_count = 1
+iterations = 1
+
+if len(sys.argv) > 1 and sys.argv[1] == "grid":
+    COORDS = (
+        (0, 0),
+        (10, 0),
+        (20, 0),
+        (30, 0),
+        (40, 0),
+        (0, 10),
+        (10, 10),
+        (20, 10),
+        (30, 10),
+        (40, 10),
+        (0, 20),
+        (10, 20),
+        (20, 20),
+        (30, 20),
+        (40, 20),
+        (0, 30),
+        (10, 30),
+        (20, 30),
+        (30, 30),
+        (40, 30),
+        (0, 40),
+        (10, 40),
+        (20, 40),
+        (30, 40),
+        (40, 40),
+    )
+    ant_count = 10
+    iterations = 100
+    coords_len = len(COORDS)
 
 
 def plot_nodes(w=12, h=8):
@@ -30,7 +59,7 @@ def plot_nodes(w=12, h=8):
         plt.plot(x, y, "g.", markersize=15)
     plt.axis("off")
     fig = plt.gcf()
-    fig.set_size_inches([w, h])
+    fig.set_size_inches(w, h)
 
 
 def plot_all_edges():
@@ -42,9 +71,16 @@ def plot_all_edges():
 
 plot_nodes()
 
-colony = AntColony(COORDS, ant_count=10, alpha=0.5, beta=1.2, 
-                    pheromone_evaporation_rate=0.40, pheromone_constant=1000.0,
-                    iterations=5)
+
+colony = AntColony(
+    COORDS,
+    ant_count=ant_count,
+    alpha=0.5,
+    beta=1.2,
+    pheromone_evaporation_rate=0.40,
+    pheromone_constant=1000.0,
+    iterations=iterations,
+)
 
 optimal_nodes = colony.get_path()
 
@@ -54,5 +90,5 @@ for i in range(len(optimal_nodes) - 1):
         (optimal_nodes[i][1], optimal_nodes[i + 1][1]),
     )
 
+plt.savefig(f"zad2_res/antc_{ant_count}_iter_{iterations}_clen_{coords_len}.png")
 
-plt.show()
